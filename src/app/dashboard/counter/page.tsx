b17d9242f03b21eea9f3aa2fc6a5a1d4613ba28b
Juan Camilo Henao Vallejo <jchenaov@postobon.com.co>
2026-02-24 13:13:46 -0500

    dashboard

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+import { CartCounter } from '../../shopping-cart/components/CartCounter';
+
+export const metadata: Metadata = {
+ title: 'Shopping Cart',
+ description: 'Counter Title',
+};
+
+export default function CounterPage() {
+
+    return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Producto en el carrito</span>
+      <CartCounter/>
+    </div>
+  );
+}
